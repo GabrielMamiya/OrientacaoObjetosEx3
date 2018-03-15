fe4ba3df9d257ce276b1921494a567dab3b3c162
Gabriel Tamura <gabriel-tamura@hotmail.com>
2018-03-14 23:06:26 -0300

    Initial check-in of module OrientacaoObjetosEx3

---
+++ Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace OrientacaoObjetosEx3
+{
+    public class Aluno
+    {
+        public string nome;
+        public double nota1, nota2, nota3;
+
+        public Aluno(string nome, double nota1, double nota2, double nota3) {
+
+            this.nome = nome;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.nota3 = nota3;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace OrientacaoObjetosEx3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Aluno Aluno1;
+
+
+
+            string nome = Console.ReadLine();
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Aluno1 = new Aluno(nome,nota1, nota2, nota3);
+
+            double NotaFInal = (Aluno1.nota1 + Aluno1.nota2 + Aluno1.nota3);
+
+            Console.WriteLine("NOTA FINAL = " + NotaFInal.ToString("F2", CultureInfo.InvariantCulture));
+
+            if(NotaFInal >= 90){
+                Console.WriteLine("Aprovado!");
+            } else if (NotaFInal < 90){
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltaram " + (NotaFInal - 60) + " Pontos!");
+            }
+
+
+        }
+    }
+}
